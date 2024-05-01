9912d6279547cb99b6d1db80f88115601d364634
Zach Serikow <76140451+sigmanotation@users.noreply.github.com>
2024-05-01 16:21:48 -0400

    Saving more updates.

--- src/attpc_engine/detector/diffusion.py
+++ src/attpc_engine/detector/diffusion.py
@@ -1,4 +1,3 @@
-import shapely
 import scipy
 import math
 
@@ -9,18 +8,60 @@ from .beam_pads import BEAM_PADS
 NUM_TB: int = 512
 PIXEL_SIZE: float = 1e-3    # m
 
-def gaussian_2d(x:float,
-                y:float,
-                mu: tuple[float, float],
-                sigma: float):
+def normal_pdf(x: float,
+               mu: float,
+               sigma: float):
+    """
+    Equation of PDF corresponding to a 1D normal distribution.
+
+    Parameters
+    ----------
+    x: float
+        Value to evaluate PDF at.
+    mu: float
+        Mean of distribution.
+    sigma: float
+        Standard deviation of distribution.
+
+    Returns
+    -------
+    float:
+        Value of distribution with the input mean and sigma
+        at the input value.
+    """
+    c1: float = 1 / math.sqrt(2 * np.pi) / sigma
+    c2: float = (-1 / 2) * ((x - mu) / sigma) ** 2
+
+    return (c1 * math.exp(c2))
+
+def bivariate_normal_pdf(point: tuple[float, float],
+                         mu: tuple[float, float],
+                         sigma: float):
+    """
+    Equation of PDF corresponding to a 2D normal distribution where sigma is the same
+    for both x and y and there is no correlation between the two variables.
+
+    Parameters
+    ----------
+    point: tuple[float, float]
+        Point to evaluate PDF at.
+    mu: tuple[float, float]
+        Mean of distribution.
+    sigma: float
+        Standard deviation of distribution. Same for both
+        x and y.
+
+    Returns
+    -------
+    float:
+        Value of distribution with the input mean and sigma
+        at the input point.
      """
-     Equation of PDF corresponding to a 2D gaussian where sigma is the same
-     for both x and y and there is no correlation between the two variables.
-     """
-     c1: float = 1 / 2 / np.pi / (sigma ** 2)
-     a1: float = (-1 / 2) * (1 / sigma ** 2) * ((x - mu[0]) ** 2) + ((y - mu[1]) ** 2)
-     
-     return (c1 * np.exp(a1))
+    c1: float = 1 / 2 / np.pi / (sigma ** 2)
+    c2: float = (-1 / 2 / sigma ** 2) * (((point[0] - mu[0]) ** 2) +
+                                          ((point[1] - mu[1]) ** 2))
+    
+    return (c1 * math.exp(c2))
 
 class diffuse_point:
     """
@@ -47,20 +88,24 @@ class diffuse_point:
                       params: Parameters):
         """
         """
-
-        #No diffusion
+        # No diffusion
         if params.detector.diffusion == (0, 0):
-            return self.do_point(params)
+            pads_hit: dict = self.do_point(params)
     
+        # At least transverse diffusion
         elif params.detector.diffusion[0] != 0:
-            # At least transverse
-            return self.do_transverse(params)
-            #do_longitudinal()
+            pads_hit: dict = self.do_transverse(params)
 
+            # Transverse and lateral diffusion
+            if params.detector.diffusion[1] != 0:
+                5
+
+        # Only lateral diffusion
         elif params.detector.diffusion[1] != 0:
-            # only lateral
-            #do_longitudinal()
-            5
+            pads_hit: dict = self.do_point(params)
+            5 
+        
+        return pads_hit
     
     def do_point(self,
                  params: Parameters):
@@ -101,10 +146,10 @@ class diffuse_point:
                       params: Parameters):
         """
         Transports all electrons created at a point in a simulated particle's
-        track to the pad plane with transverse diffusion. This is done by
+        track to the pad plane with transverse diffusion applied. This is done by
         creating a square mesh roughly centered on the point where the electrons are
         created. The mesh extends from -3 sigma to 3 sigma in both the 
-        x and y directions. The size of one pixel is the mesh is controlled 
+        x and y directions. The size of one pixel of the mesh is controlled 
         by PIXEL_SIZE at the top of this file. Each pixel of the mesh has a 
         number of electrons in it calculated from the bivariate normal
         distribution PDF.
@@ -133,18 +178,12 @@ class diffuse_point:
         if steps < 1:
             return self.do_point(params)
 
-        # Create mesh and calculate number of electrons in each pixel
+        # Create mesh
         xlims = (mesh_centerx - 3 * self.sigma_t, mesh_centerx + 3 * self.sigma_t)
         ylims = (mesh_centery - 3 * self.sigma_t, mesh_centery + 3 * self.sigma_t)
         xmesh, ymesh = np.meshgrid(np.linspace(*xlims, steps), np.linspace(*ylims, steps))
-        points = np.stack((xmesh, ymesh), axis=-1)
-        pdf = scipy.stats.multivariate_normal.pdf(points,
-                                                  mean=[mesh_centerx, mesh_centery],
-                                                  cov=self.sigma_t**2)
-        pdf *= (PIXEL_SIZE ** 2) * self.electrons
-        pdf = pdf.astype(int).ravel()
 
-        for idx, pixel in enumerate(np.column_stack((xmesh.ravel(), ymesh.ravel()))):
+        for pixel in np.column_stack((xmesh.ravel(), ymesh.ravel())):
             # Find pad number of hit pad, if it exists
             index: tuple[int, int] | None = position_to_index(params,
                                                               pixel)
@@ -155,7 +194,10 @@ class diffuse_point:
             # Ensure electron hits pad plane and hits a non-beam pad
             if pad != -1 and pad not in BEAM_PADS:
                 trace: np.ndarray = np.zeros(NUM_TB)
-                trace[math.floor(self.time)] = pdf[idx]
+                trace[math.floor(self.time)] = (bivariate_normal_pdf(pixel,
+                                                                    self.center,
+                                                                    self.sigma_t) *
+                                                (PIXEL_SIZE ** 2) * self.electrons)
 
                 if pad not in pads_hit:
                     pads_hit[pad] = trace
@@ -166,19 +208,51 @@ class diffuse_point:
         return pads_hit
 
     def do_longitudinal(self,
-                        results: dict[dict]):
+                        params: Parameters,
+                        pads_hit: dict):
         """
         """
 
+        mesh_centerx: float = self.center[0]
+        mesh_centery: float = self.center[1]
+
+        # Number of pixels along the x and y axes of the mesh
+        steps: float = int(np.ceil((2 * 3 * self.sigma_l) / PIXEL_SIZE)) + 1
+
+        # Diffusion is smaller than a pixel, so treat it as a point
+        if steps < 1:
+            return self.do_point(params)
+
+        # Create mesh
+        xlims = (mesh_centerx - 3 * self.sigma_t, mesh_centerx + 3 * self.sigma_t)
+        ylims = (mesh_centery - 3 * self.sigma_t, mesh_centery + 3 * self.sigma_t)
+        xmesh, ymesh = np.meshgrid(np.linspace(*xlims, steps), np.linspace(*ylims, steps))
+
+        for pixel in np.column_stack((xmesh.ravel(), ymesh.ravel())):
+            # Find pad number of hit pad, if it exists
+            index: tuple[int, int] | None = position_to_index(params,
+                                                              pixel)
+            if index is None:
+                continue
+            pad: int = params.pad_map[index[0], index[1]]
+
+            # Ensure electron hits pad plane and hits a non-beam pad
+            if pad != -1 and pad not in BEAM_PADS:
+                trace: np.ndarray = np.zeros(NUM_TB)
+                trace[math.floor(self.time)] = (bivariate_normal_pdf(pixel,
+                                                                    self.center,
+                                                                    self.sigma_t) *
+                                                (PIXEL_SIZE ** 2) * self.electrons)
+
+                if pad not in pads_hit:
+                    pads_hit[pad] = trace
+
+                else:
+                    pads_hit[pad] += trace
+
+        return pads_hit
 
 
-    def do_diffusion(self,
-                     ):
-        """
-        """
-        #case where we want both lateral and longitudinal
-        results_t = self.do_transverse()
-        results_l = self.do_longitudinal(results_t)
 
 def position_to_index(params: Parameters,
                       position: tuple[float, float]):
