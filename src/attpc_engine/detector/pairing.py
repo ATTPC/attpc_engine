dbdaf30167e956ec1e011e356c2f86d266042fe1
gwm17 <gordonmccann215@gmail.com>
2024-09-16 11:40:16 -0400

    Document pairing functions

--- src/attpc_engine/detector/pairing.py
+++ src/attpc_engine/detector/pairing.py
@@ -4,6 +4,24 @@ from numba import njit
 
 @njit
 def pair(tb: int, pad: int) -> int:
+    """Combine two integers into one integer
+
+    Uses Szudzik pairing function to combine Time Bucket and Pad
+    into a id.
+
+    Parameters
+    ----------
+    tb: int
+        The time bucket of the data
+    pad: int
+        The pad id of the data
+
+    Returns
+    -------
+    int
+        The id of this data from the tb and pad
+
+    """
     if tb < 0 or pad < 0:
         return -1
     return tb * tb + tb + pad if tb == max(tb, pad) else pad * pad + tb
@@ -11,6 +29,22 @@ def pair(tb: int, pad: int) -> int:
 
 @njit
 def unpair(id: int) -> tuple[int, int]:
+    """Inverse of the pairing function
+
+    This is the inverse pairing function for Szudzik pairing
+
+    Parameters
+    ----------
+    id: int
+        A id generated from the pair function
+
+    Returns
+    -------
+    tuple[int, int]
+        A pair of numbers which was used to generate the original id.
+        In our case the first is the time bucket, the second is the pad id.
+        Note that the order is important!
+    """
     if id < 0:
         return (-1, -1)
 
